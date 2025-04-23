ce4d9cb9022c67e258a7041f621f86ea98707a1a
ChristopheLecroq <christophe.lecroq@gmail.com>
2025-04-23 10:31:32 +0200

    feat(fibonnaci): use big.Int type

--- fibonacci/main.go
+++ fibonacci/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"math/big"
+	"time"
+)
+
 func FibonacciRecursive(n int) int {
 	if n <= 1 {
 		return n
@@ -32,9 +38,34 @@ func FibonacciDynamic(n int) int {
 	return fib[n]
 }
 
+var cache = make(map[int]*big.Int, 1000)
+
+func FibonacciMemo(n int) *big.Int {
+	if n <= 1 {
+		return big.NewInt(int64(n))
+	}
+
+	if val, exists := cache[n]; exists {
+		return val
+	}
+
+	cache[n] = new(big.Int).Add(FibonacciMemo(n-1), FibonacciMemo(n-2))
+	return cache[n]
+}
+
 func main() {
-	n := 40
-	println("FibonacciRecursive:", FibonacciRecursive(n))
+	//start := time.Now()
+	//i:= 1000
+	//fmt.Printf("%d :  %d (%d ms) \n", i, FibonacciMemo(i), time.Since(start).Milliseconds())
+
+	for i := 0; i < 10000; i += 1 {
+		start := time.Now()
+		fmt.Printf("%d :  %d (%d ms) \n", i, FibonacciMemo(i), time.Since(start).Milliseconds())
+	}
+
+	/* n := 40 */
+	/* println("FibonacciRecursive:", FibonacciRecursive(n)) */
+	/* println("FibonacciMemo:", FibonacciMemo(n)) */
 	/* println("FibonacciLoop:", FibonacciLoop(n)) */
 	/* println("FibonacciDynamic:", FibonacciDynamic(n)) */
 }
